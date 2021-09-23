16f38a29ffa427460ae76b7a9143bd4810f4253e
OhJeongHo <ojh5015@gmail.com>
2021-09-23 23:34:47 +0900

    .

---
+++ Csharp_book/String/01.Intro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Length   -   문자열 길이 값 반환
+            // ToUpper()-   전부 대문자로 변환
+            // ToLower()-   전부 소문자로 변환
+            // Trim()   -   양쪽 공백을 잘라냄
+            // Replace(원본문자열, 대상문자열)    -   원본 문자열을 대상문자열로 변경
+            // Substring(문자열인덱스, 길이)    -   지정된 문자열 인덱스로부터 길이만큼 반환
+        }
+    }
+}
