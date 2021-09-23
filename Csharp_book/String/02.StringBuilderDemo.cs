1682619330684c9afb96fe841eb57e03044724f4
OhJeongHo <ojh5015@gmail.com>
2021-09-24 00:14:23 +0900

    .

---
+++ Csharp_book/String/02.StringBuilderDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace String
+{
+    class StringBuilderDemo
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("January\n");
+            sb.Append("February\n");
+            sb.Append("March");
+
+            Console.WriteLine(sb);
+        }
+        
+
+    }
+}
---
+++ Csharp_book/String/03.StringPerformance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace String
+{
+    class StringBuilderPerformance
+    {
+        static void Main()
+        {
+            DateTime start = DateTime.Now;
+
+            string msg = "";
+            for (int i = 0; i < 10000; i++)
+            {
+                msg += "안녕하세요.";
+            }
+
+            DateTime end = DateTime.Now;
+            Double exec = (end - start).TotalMilliseconds;
+            Console.WriteLine(exec);
+        }
+    }
+}
