537894a33feb463f4ec8db4f67d25815c70c02c0
OhJeongHo <ojh5015@gmail.com>
2021-10-03 14:29:29 +0900

    .

---
+++ Csharp_book/Namespace/01.Info.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Namespace
+{
+    class Program
+    {
+        /*
+         * 프로그램의 규모가 커질 때 클래스 이름이 충돌하는 것을 방지하도록 클래스를 모아서 관리하는 개념.
+         * 네임스페이스를 달리하여 이름이 동일한 클래스를 하나의 프로젝트에서 사용할 수 있다.
+         * 
+         * 네임스페이스는 여러번 계층형으로 구분해서 사용할 수 있다.
+         * 각 네임스페이스 안에는 클래스 정의 코드가 들어올 수 있다.
+         * 
+         * namespace AllCar
+         * {
+         *      namespace MyCar { }
+         *      namespace YourCar{ }
+         * }
+         * 
+         * 위와 같은 네임스페이스 중첩 구문은 마침표 구문을 사용하여 표현할 수 있다.
+         * namespace AllCar.MyCar { }
+         * namespace AllCar.YourCar { }
+         */
+    }
+}
---
+++ Csharp_book/Namespace/04.TypeDefinitionWithUsing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TypeDefinitionWithUsing
+{
+    // Gilbut.Education.CSharp.Lecture 형식을 Project 별칭으로 줄여 사용
+    using Project = Gilbut.Education.CSharp.Lecture;
+
+    public class TypeDifinitionWithUsing
+    {
+        public static void Main(string[] args)
+        {
+            Gilbut.Education.CSharp.Lecture l = new Gilbut.Education.CSharp.Lecture();  // 기본 호출
+            Console.WriteLine(l);
+
+            Project p = new Project();  // using 지시문 사용 호출
+            Console.WriteLine(p);
+        }
+    }
+}
+
+namespace Gilbut
+{
+    namespace Education
+    {
+        namespace CSharp
+        {
+            public class Lecture
+            {
+                public override string ToString()
+                {
+                    return "Lecture 클래스 호출됨";
+                }
+            }
+        }
+    }
+}
