ffc52290216185a4dea188170fbb3595e0a943b6
Oh Jeong Ho <ojh5015@gmail.com>
2021-10-09 02:03:22 +0900

    .

---
+++ Csharp_book/Method/12.NamedArgument.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Class1
+    {
+        static int Sum(int first = 10, int second = 20)
+        {
+            return first + second;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(Sum(3, 5));   // 8
+
+            Console.WriteLine(Sum());   // 30
+
+            Console.WriteLine(Sum(40)); // 60
+
+            Console.WriteLine(Sum(100, 200));   // 300
+
+            Console.WriteLine(Sum(first: 5, second: 4));    // 9
+
+            Console.WriteLine(Sum(second: 3, first: 2));    // 5
+
+            Console.WriteLine(Sum(second: 50)); // 60
+        }
+    }
+}
---
+++ Csharp_book/Method/13.NamedArgument2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Class1
+    {
+        public class Messenger
+        {
+            public void PrintMessage(string message, string prefix = "", string suffix = "")
+            {
+                Console.WriteLine($"{message}{prefix}{suffix}");
+            }
+        }
+
+        static void Main()
+        {
+            Messenger messenger = new Messenger();
+            messenger.PrintMessage("My");
+
+            messenger.PrintMessage(prefix: "Oh", message: "My");
+
+            messenger.PrintMessage(prefix: "Oh", message: "My", suffix: "God");
+        }
+    }
+}
