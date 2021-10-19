8d7a3e107e3b07f3245601d54a20ee8cc1aea73e
55JeongHo <oh5093@newto.co.kr>
2021-10-19 14:48:06 +0900

    .

--- Csharp_book/Inheritance/04.ConstructorBase.cs
+++ Csharp_book/Inheritance/04.ConstructorBase.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Inheritance
+namespace ConstructorBase
 {
-    class Class1
+    class parent
     {
+        // 매개변수로 넘어온 값 출력
+        public parent(string message) => Console.WriteLine(message);
+    }
+
+    class Child : parent
+    {
+        // 1. base()를 사용하여 부모 클래스의 생성자 호출
+        public Child(string message) : base(message) { }
+    }
+
+    class ConstructorBase
+    {
+        static void Main()
+        {
+            string message = "자식 클래스의 생성자를 호출할 때 부모 클래스의 생성자로 전달";
+            var child = new Child(message);
+        }
     }
 }
