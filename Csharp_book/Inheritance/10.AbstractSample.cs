ed2c2ae8dace9ce874db8fb17a018228ca780644
55JeongHo <oh5093@newto.co.kr>
2021-10-19 15:52:02 +0900

    .

---
+++ Csharp_book/Inheritance/10.AbstractSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+abstract class GeneralManager   // 부장님 클래스 생성
+{
+    public abstract void SayHumor();    // 메소드 본문 생략
+}
+
+class Person : GeneralManager   // 부장님 클래스를 상속하는 사람 클래스 생성
+{
+    public override void SayHumor()
+    {
+        Console.WriteLine("1+1은? 노가다!");
+    }
+}
+
+class Test
+{
+    static void Main()
+    {
+        var person = new Person();
+        person.SayHumor();
+    }
+}
---
+++ Csharp_book/Inheritance/11.AbstractSample2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public abstract class Shape // 1. 추상 클래스
+{
+    public abstract double GetArea();   // 2. 추상 멤버 : 추상 메소드
+}
+
+public class Square : Shape // 3. 추상 클래스를 상속하는 클래스
+{
+    private int _size;
+    public Square(int size)
+    {
+        _size = size;
+    }
+
+    // 부모 클래스인 Shape 추상 클래스의 추상 멤버인 GetArea() 메소드를 구현
+    public override double GetArea()
+    {
+        return _size * _size;
+    }
+}
+
+class Test
+{
+    static void Main()
+    {
+        Square square = new Square(10);   // a. 자신의 이름으로 인스턴스 생성
+        Console.WriteLine(square.GetArea());
+
+        Shape shape = new Square(5);    // b. 부모의 이름으로 인스턴스 생성
+        Console.WriteLine(shape.GetArea());
+
+    }
+}
